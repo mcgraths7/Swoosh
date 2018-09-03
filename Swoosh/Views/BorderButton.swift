a1ca210b11c231c8e1c4cdad1dbedbe7d5a00a8a
mcgraths7 <mcgrath.steven7@gmail.com>
2018-09-03 17:19:45 -0400

    Added custom button "BoderButton" to add  white border to button

---
+++ Swoosh/Views/BorderButton.swift
@@ -0,0 +1,19 @@
+//
+//  BorderButton.swift
+//  Swoosh
+//
+//  Created by Steven McGrath on 9/3/18.
+//  Copyright © 2018 Steven McGrath. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+
+}
