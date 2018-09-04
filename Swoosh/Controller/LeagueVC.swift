ace45ba223b25d9a5180d114ee075cf53fd7c2dc
mcgraths7 <mcgrath.steven7@gmail.com>
2018-09-04 14:51:18 -0400

    disabled next button unless a league is selected, added player model to store league and skill level

--- Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Controller/LeagueVC.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    var player: Player!
+    @IBOutlet weak var nextButton: BorderButton!
+    @IBOutlet weak var menButton: BorderButton!
+    @IBOutlet weak var womenButton: BorderButton!
+    @IBOutlet weak var coedButton: BorderButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        player = Player()
         // Do any additional setup after loading the view.
     }
     
@@ -22,6 +26,24 @@ class LeagueVC: UIViewController {
     @IBAction func onNextButtonTapped(_ sender: BorderButton) {
         performSegue(withIdentifier: "leagueToFinish", sender: self)
     }
-
-
+    
+    @IBAction func onMenTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "mens")
+        menButton.layer.borderColor = #colorLiteral(red: 0.9981321692, green: 1, blue: 0, alpha: 1)
+    }
+    
+    @IBAction func onWomenTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "womens")
+        womenButton.layer.borderColor = #colorLiteral(red: 0.9981321692, green: 1, blue: 0, alpha: 1)
+    }
+    
+    @IBAction func onCoedTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "co-ed")
+        coedButton.layer.borderColor = #colorLiteral(red: 0.9981321692, green: 1, blue: 0, alpha: 1)
+    }
+    
+    func selectLeague(leagueType type: String) {
+        player.desiredLeague = type
+        nextButton.isEnabled = true
+    }
 }
